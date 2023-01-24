fbcc6b859ccfcf10d15a1fd4d2291888f73e8ee3
Yakov Egortsev <yakov@egortsev.ru>
2023-01-25 02:11:39 +0300

    Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Console.ReadLine();
+Console.WriteLine("Введите число B");
+int numberB = Console.ReadLine();
+//int numberA = 1;
+//int numberB = 3;
+int result = numberA + numberB;
+Console.WriteLine("Сумма чсел A и B = ");
+Console.WriteLine(result);
